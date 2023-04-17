1c979e3b3362b1de095e401ea9b4acf86b29c11d
Adam Shepard <adam.w.gangl@gmail.com>
2023-04-17 13:59:27 -0500

    completed basic mockup of the title page.

--- src/components/TitlePage/TitlePage.jsx
+++ src/components/TitlePage/TitlePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StarfallAdAstra from '../img/StarfallAdAstra.png'
+import { useHistory } from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,11 +9,16 @@ import StarfallAdAstra from '../img/StarfallAdAstra.png'
 
 function TitlePage() {
 
+const history = useHistory()
+
+const handleClick = () => {
+  history.push('/')
+}
+
   return (
-    <div className="container">
-      <div>
-      <img src={StarfallAdAstra} alt="Starfall Ad Astra"  style={{width: '80%', height: '100%', objectFit: 'cover'}}/>
-      </div>
+    <div className="container" style={{ display: 'flex', justifyContent: 'center', position: 'relative' }}> 
+      <img src={StarfallAdAstra} alt="Starfall Ad Astra" style={{ width: '80%', height: '100%', objectFit: 'cover' }} />
+      <button style={{ position: 'absolute', top: '90%', left: '50%', transform: 'translate(-50%, -50%)' }} onClick={handleClick}> Start Your Adventure</button>
     </div>
   );
 }
