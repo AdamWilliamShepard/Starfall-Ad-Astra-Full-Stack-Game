0146cb0b637ddfdedf120ff9c07a9f9ca1c28e51
Adam Shepard <adam.w.gangl@gmail.com>
2023-04-17 14:02:25 -0500

    cleaned up comment code.

--- src/components/TitlePage/TitlePage.jsx
+++ src/components/TitlePage/TitlePage.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import StarfallAdAstra from '../img/StarfallAdAstra.png'
 import { useHistory } from 'react-router-dom';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
-
 function TitlePage() {
 
 const history = useHistory()
